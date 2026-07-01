298025a2778e21c5f90567dd4dfe8a720dfbcf6f
barboss <bayankov.kirill@yandex.ru>
2026-07-01 13:48:04 +0300

    edited one if

--- server/physics_engine.py
+++ server/physics_engine.py
@@ -28,7 +28,7 @@ def calculate_puck_wall_collision(puck : Puck):
         ((puck.position.second <= (TOP_WALL - PUCK_RADIUS) or           #проверка нахождения шайбы в воротах + коллизия с их левой стенкой
           puck.position.second >= (DOWN_WALL + PUCK_RADIUS)) and 
          puck.position.first <= (GOAL_LEFT + PUCK_RADIUS))
-         
+         or
          ((puck.position.first >= (RIGHT_WALL - PUCK_RADIUS)) or      #проверка на коллизию с правой стенкой
           ((puck.position.second <= (TOP_WALL - PUCK_RADIUS) or         #проверка на нахождение шайбы в воротах + коллизия с их правой стенкой
           puck.position.second >= (DOWN_WALL + PUCK_RADIUS)) and 
