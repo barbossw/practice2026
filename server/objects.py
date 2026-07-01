5e28814d75fb2cfd5f134b70c004c243be66eee5
a1eksiy <aleksejvyblov@gmail.com>
2026-07-01 20:40:31 +0300

    final websocket changes. websockets diffetentiate players connections from each other. no race condition in possible in connect() and disconnect() with asyncio.Lock(). WebHandler status updates correctly after each change. more than 2 players are rejected immediatly

--- server/objects.py
+++ server/objects.py
@@ -1,9 +1,10 @@
 from dataclasses import dataclass
 from typing import overload
 from fastapi import WebSocket, WebSocketDisconnect
-from enum import Enum
+from enum import IntEnum
 import constants
 import math
+import asyncio
 
 @dataclass
 class Pair:
@@ -52,6 +53,8 @@ class Pair:
 
 def normalize_vector(vector : Pair) -> Pair:
         length = math.sqrt(vector.first ** 2 + vector.second ** 2)
+        if length == 0:
+             return Pair(0, 0)
         new_vector = Pair(vector.first / length, vector.second / length)
         return new_vector
 
@@ -98,29 +101,75 @@ class GameState:
      score : Pair
 
 
-class Status(Enum):
+class Status(IntEnum):
      NO_PLAYERS = 0
      WAITING_FOR_OTHER_PLAYER = 1
      READY = 2
 
 
 
-class WebSockerHandler:
-     connections : set[WebSocket]
-     #status : Status = 0
+class WebSocketHandler:
+     player1 : WebSocket | None
+     player2 : WebSocket | None
+     status : Status
 
      def __init__(self):
-          self.connections = set()
+          self.player1 = None
+          self.player2 = None
+          self.status = Status.NO_PLAYERS
+          self.lock = asyncio.Lock()
 
 
-     async def connect(self, websocket : WebSocket):
+     def number_of_connected_players(self) -> int:
+          connected_players = 0
+          if isinstance(self.player1, WebSocket):
+               connected_players += 1
+          if isinstance(self.player2, WebSocket):
+               connected_players += 1
+          return connected_players
+
+
+     async def connect(self, websocket : WebSocket) -> bool:
           await websocket.accept()
-          self.connections.add(websocket)
+
+          async with self.lock:
+               if self.player1 is None:
+                    self.player1 = websocket
+               elif self.player2 is None:
+                    self.player2 = websocket
+               else:
+                    await websocket.close()
+                    return False
+               self.status = Status(self.number_of_connected_players())
+               return True
 
 
      async def disconnect(self, websocket : WebSocket):
-          self.connections.discard(websocket)
 
-     #careful with message data type !!
-     async def send_to_player(self, websocket : WebSocket, message : dict):
-          await websocket.send_json(message)
+          async with self.lock:
+               if websocket is self.player1:
+                    self.player1 = None
+               elif websocket is self.player2:
+                    self.player2 = None
+               self.status = Status(self.number_of_connected_players())
+               return
+
+
+
+     #message data type - dict
+     #need to serialize GameState to dict with asdict(game_state)
+     async def send_to_player1(self, message : dict):
+          if isinstance(self.player1, WebSocket):
+               await self.player1.send_json(message)
+          else:
+               raise RuntimeError("Player 1 is not connected. Trying to send a message with no connection")          
+
+     async def send_to_player2(self, message : dict):
+          if isinstance(self.player2, WebSocket):
+               #reverse data across the (0,0) coordinate here
+               await self.player2.send_json(message)
+          else:
+               raise RuntimeError("Player 2 is not connected. Trying to send a message with no connection")     
+          
+     
+     
--- server/source.py
+++ server/source.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from contextlib import asynccontextmanager
+from objects import WebSocketHandler
 
-app = FastAPI()
+
+
+@asynccontextmanager
+async def lifespan_func(app : FastAPI):
+    app.state.web_handler = WebSocketHandler()
+
+    yield
+
+
+
+app = FastAPI(lifespan = lifespan_func)
+
+
+
+
+
 
 
 @app.get("/")
@@ -11,4 +28,20 @@ def root():
 
 @app.websocket("/ws_connect")
 async def websocket_connect(websocket : WebSocket):
-    await websocket.accept()
+
+    handler = websocket.app.state.web_handler
+    accepted = await handler.connect(websocket)
+    if not accepted:
+        return {"message" : "Failed to establish connection - the game is full"}
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            if websocket is handler.player1:
+                #process for player 1 here
+                pass
+            elif websocket is handler.player2:
+                #process for player 2 here - invert across (0,0)
+                pass
+    except WebSocketDisconnect:
+        handler.disconnect(websocket)
