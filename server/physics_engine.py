d8b56ddf0f510fd2cd03e7c1eb7b0fe3c800dfaf
barboss <bayankov.kirill@yandex.ru>
2026-07-07 23:52:28 +0300

    changes in puck-wall collision and change speed limit

--- server/physics_engine.py
+++ server/physics_engine.py
@@ -47,12 +47,12 @@ def calculate_puck_wall_collision(puck : Puck):
     if ((puck.position.first <= (LEFT_WALL + PUCK_RADIUS)) or           #проверка на коллизию с левой стенкой
         ((puck.position.second >= (TOP_WALL - PUCK_RADIUS) or           #проверка нахождения шайбы в воротах + коллизия с их левой стенкой
           puck.position.second <= (DOWN_WALL + PUCK_RADIUS)) and 
-         puck.position.first <= (GOAL_LEFT + PUCK_RADIUS))
+         puck.position.first >= (GOAL_LEFT + PUCK_RADIUS))
          or
          ((puck.position.first >= (RIGHT_WALL - PUCK_RADIUS)) or      #проверка на коллизию с правой стенкой
           ((puck.position.second >= (TOP_WALL - PUCK_RADIUS) or         #проверка на нахождение шайбы в воротах + коллизия с их правой стенкой
           puck.position.second <= (DOWN_WALL + PUCK_RADIUS)) and 
-         puck.position.first >= (GOAL_RIGHT - PUCK_RADIUS)))):
+         puck.position.first <= (GOAL_RIGHT - PUCK_RADIUS)))):
         normal_vector_from_wall = Pair(1, 0)
         new_puck_speed_vector = puck_speed_vector - normal_vector_from_wall * (puck_speed_vector * normal_vector_from_wall) * 2
 
